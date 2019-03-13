d759a310781a7833c7eba8a0b3e59eeaef072afd
unknown <unknown@example.com>
2019-03-13 11:37:33 +0800

    03013

---
+++ pages/consume/consume.js
@@ -0,0 +1,163 @@
+var app = getApp()
+Page({
+  data: {
+    orderList: [],
+    page: 1,//默认第一页    
+    isHideLoadMore: true,
+    nomore: false,
+    uid: ''
+  },
+  bindViewTap_sy: function () {
+    wx.switchTab({
+      url: '../home/home'
+    })
+  },
+  bindViewTap_xm: function () {
+    wx.switchTab({
+      url: '../classify/classify'
+    })
+  },
+  bindViewTap_cz: function () {
+    wx.switchTab({
+      url: '../charge/charge'
+    })
+  },
+  bindViewTap_me: function () {
+    wx.switchTab({
+      url: '../personal/personal'
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    // 页面显示
+    var that = this
+    this.data.uid = wx.getStorageSync('openid')
+    console.log('第一次 页面 isHideLoadMore=' + this.data.isHideLoadMore)
+    // 这里要注意，微信的scroll-view必须要设置高度才能监听滚动事件，所以，需要在页面的onLoad事件中给scroll-view的高度赋值
+    //原文：https://blog.csdn.net/qq_35695041/article/details/80236489 
+    wx.getSystemInfo({
+
+      success: function (res) {
+
+        that.setData({
+
+          scrollHeight: res.windowHeight
+
+        });
+
+      }
+    })
+    this.loadmyorders();
+  },
+
+  //下拉刷新
+  onPullDownRefresh: function () {
+    console.log('下拉刷新')
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    this.setData({
+      page: 1,
+      nomore: false,
+      orderList: []
+    });
+    this.loadmyorders()
+
+
+    wx.hideNavigationBarLoading() //完成停止加载
+    wx.stopPullDownRefresh() //停止下拉刷新
+    /*---------------------  
+    原文：https://blog.csdn.net/u012927188/article/details/73369201
+
+  */
+  },
+
+  /**       * 页面上拉触底事件的处理函数       */
+  onReachBottom: function () {
+
+
+    console.log('加载更多')
+    if (!this.data.nomore) {
+      this.loadmyorders()
+    }
+  },
+  loadmyorders: function () {
+    //if (ifLoadMore) {
+    console.log('load loadmyorders')
+    console.log('进入时  isHideLoadMore=' + this.data.isHideLoadMore)
+    //console.log('page=' + this.data.page)
+    this.setData({
+      isHideLoadMore: false
+    })
+    wx.showLoading({
+      title: '加载中...'
+    });
+    //调取商品信息
+    let orderList = [];
+    var that = this;
+    wx.request({
+      url: app.globalData.urlPath + 'getmyconsumeList.php',
+      data: {
+        page: this.data.page,
+        uid: this.data.uid,
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success: function (res) {
+        //从数据库获取用户信息     
+        //判断是否为空
+        //console.log(res)
+        //console.log(res.data)
+        orderList = that.data.orderList
+        //加载更多
+        if (res.data.result.length > 0) {
+          var page = that.data.page + 1;
+          for (var i = 0; i < res.data.result.length; i++) {
+            orderList.push(res.data.result[i]);
+          }
+
+          that.setData({
+            // loadingCount: orderList.length,
+            orderList: orderList,
+            page: page
+          });
+
+        }
+        else {
+          //没有更多新内容
+          that.setData({
+            nomore: true
+          })
+
+          wx.showToast({
+            title: '没有更多记录！',
+            icon: 'loading',
+            duration: 2000
+          })
+        }
+      },
+      complete: function () {
+        wx.hideLoading();
+      }
+    })
+    this.setData({
+      isHideLoadMore: true
+    })
+    console.log('结束时 isHideLoadMore=' + this.data.isHideLoadMore)
+
+  },//end of loadmyorders
+
+
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -2,6 +2,9 @@
 var app = getApp()
 Page({
   data: {
+    uid:'',
+    title:'',
+    balance:'',
     userInfo: {},
     hasUserInfo: false,//是否有用户信息，默认否
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -11,13 +14,15 @@ Page({
   
   onLoad: function () {
     //console.log(' Personal.js app.globalData.userInfo=' + app.globalData.userInfo)
-    
+    this.data.uid = wx.getStorageSync('openid')
+
     if (app.globalData.userInfo) {
       //1.1判断是否保存了全局用户信息，如果有就直接调取
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      this.get_data()
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       console.log('跳到授权页面')
@@ -34,6 +39,33 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  get_data() {
+    var self = this;
+    wx.showLoading({
+      title: '加载中...'
+    });
+    wx.request({
+      url: app.globalData.urlPath + 'getbalance.php',
+      method: "POST",
+      data: {
+        uid: this.data.uid,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success: function (res) {  //后端返回的数据
+        var data = res.data;
+        // console.log(data.result);
+        self.setData({
+          balance: data.result.balance,
+          tname: data.result.name
+        })
+      },
+      complete: function () {
+        wx.hideLoading();
+      }
+    });
   },
   myAddress: function (e) {
     wx.navigateTo({ url: '../addressList/addressList' });
